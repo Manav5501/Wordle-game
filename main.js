9b484314e77e7416aec9044b44a4902e90dc7bf9
Manav5501 <thavarsetiya5501@gmail.com>
2022-05-19 22:24:48 +0530

    game done

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
+// Setup GameData
 const wordSet = new Set();
 fetch("/wordslist.txt")
   .then((response) => response.text())
   .then((text) => text.split("\n"))
-  .then(wordlist=>{
-    for(const word in wordlist) {
-      wordSet.add(wordlist[word])
-    }
-  })
+  .then((wordlist) => {
+    wordlist.forEach((word) => wordSet.add(word.toUpperCase()));
+  });
+
+var answerWord;
+fetch("/acceptable_answers.txt")
+  .then((resp) => resp.text())
+  .then((text) => text.split("\n"))
+  .then(
+    (answerlist) =>
+      (answerWord = answerlist[Math.floor(Math.random() * answerlist.length)])
+  );
 
 console.log(wordSet);
+console.log(answerWord);
 
+//Setup Input Listeners
 keys = document.getElementsByClassName("key");
 for (i = 0; i < keys.length; i++) {
   keys[i].onclick = (event) =>
@@ -18,6 +28,7 @@ for (i = 0; i < keys.length; i++) {
 
 document.onkeydown = (event) => gameinput(event.key.toUpperCase());
 
+// Setup GameBoard
 const board = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -26,13 +37,8 @@ const board = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
-used_keys = [];
-yellow_keys = [];
-green_keys_index = [];
-attempt = 0;
-letterPos = 0;
 
-function mainloop() {
+function redrawGrid() {
   rows = document.getElementsByClassName("row");
   for (r = 0; r < 6; r++) {
     letters = rows[r].getElementsByClassName("letter");
@@ -42,16 +48,25 @@ function mainloop() {
   }
 }
 
+var attempt = 0;
+var letterPos = 0;
+
+var used_keys = [];
+var yellow_keys = [];
+var green_keys_index = [];
+var matches = ["", "", "", "", ""];
+
+
 function gameinput(key) {
   console.log(key);
+
+  // Press Alphabet
   if (key.match(/^[A-Z]{1}$/) && letterPos < 5) {
     board[attempt][letterPos] = key;
     letterPos++;
   }
-  if (letterPos === 5 && key === "ENTER") {
-    attempt++;
-    letterPos = 0;
-  }
+
+  // Press Backspace
   if (
     (key === "BACKSPACE" || key === "DEL" || key == "DELETE") &&
     letterPos > 0
@@ -59,5 +74,74 @@ function gameinput(key) {
     board[attempt][letterPos - 1] = "";
     letterPos--;
   }
-  mainloop();
+
+  // Submit Guess
+  if (key === "ENTER" && letterPos === 5) {
+
+    let word = board[attempt].join("").toUpperCase();
+    if (!wordSet.has(word)) {
+      alert("Invalid word"); 
+      return;
+    };
+
+    /* COLOR KEYS */
+    // grey keys
+    for(i=0;i<5;i++) {
+      document.getElementById(word[i]).classList.add("bg-used")
+    }
+    // green keys 
+    for (i = 0; i < 5; i++) {
+      if (word[i] === answerWord[i]) {
+        matches[i] = "g";
+        document.getElementById(word[i]).classList.add("bg-green");
+        green_keys_index.push(i);
+      }
+    }
+    // yellow keys
+    for (i = 0; i < 5; i++) {
+      for (j = 0; j < 5; j++) {
+        if (green_keys_index.includes(j) || yellow_keys.includes(j)) continue; //console.log("Match at ", i, j);
+        if (word[i] == answerWord[j]) {
+          matches[i] = "y";
+          yellow_keys.push(j);
+          document.getElementById(word[i]).classList.add("bg-yellow");
+        }
+      }
+    }
+    
+    for (i = 0; i < 5; i++) {
+      document
+        .getElementsByClassName("row")
+        [attempt].getElementsByClassName("letter")
+        [i].classList.add(
+          matches[i] == "y"
+            ? "bg-yellow"
+            : matches[i] == "g"
+            ? "bg-green"
+            : "bg-used"
+        );
+    }
+
+    /* WIN CONDITION */
+    if (word === answerWord ) {
+      endgame("win")
+      return;
+    } else if (attempt == 5) {
+      endgame("lose")
+      return;
+    }
+
+    /* Setup for next attempt */
+    attempt++;
+    letterPos = 0;
+    green_keys_index = [];
+    yellow_keys = [];
+    matches = [];
+  }
+
+  redrawGrid();
+}
+
+function endgame(result) {
+  alert(result)
 }
